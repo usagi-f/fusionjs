ee0e38544fa14ac1f9fed6ccc449ddb1ada0e526
ishibashi-keita <ishibashi-keita1@dmm.com>
2020-07-09 10:33:01 +0900

    update: packages

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import App from 'fusion-react'
 import Router from 'fusion-plugin-react-router'
+import UniversalEvents, { UniversalEventsToken } from 'fusion-plugin-universal-events';
 import Styletron from 'fusion-plugin-styletron-react'
 import Redux, {
   ReduxToken,
@@ -17,6 +18,7 @@ export default () => {
   const app = new App(root)
   app.register(Styletron)
   app.register(Router)
+  app.register(UniversalEventsToken, UniversalEvents)
   app.register(ReduxToken, Redux)
   __NODE__ && app.register(GetInitialStateToken, initialState)
   app.register(ReducerToken, reducer)
