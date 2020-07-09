08065247ec6958dc73244ab10ec7d686ab7d7355
ishibashi-keita <ishibashi-keita1@dmm.com>
2020-07-09 10:57:15 +0900

    remove: redux

--- src/main.js
+++ src/main.js
@@ -1,26 +1,13 @@
 // @flow
 
-import React from 'react'
 import App from 'fusion-react'
 import Router from 'fusion-plugin-react-router'
-import UniversalEvents, { UniversalEventsToken } from 'fusion-plugin-universal-events';
 import Styletron from 'fusion-plugin-styletron-react'
-import Redux, {
-  ReduxToken,
-  ReducerToken,
-  GetInitialStateToken,
-} from 'fusion-plugin-react-redux'
 import root from './root.js'
-import initialState from './redux/initialState'
-import reducer from './redux/index'
 
 export default () => {
   const app = new App(root)
   app.register(Styletron)
   app.register(Router)
-  app.register(UniversalEventsToken, UniversalEvents)
-  app.register(ReduxToken, Redux)
-  __NODE__ && app.register(GetInitialStateToken, initialState)
-  app.register(ReducerToken, reducer)
   return app
 }
